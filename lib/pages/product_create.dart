65ad66bf8eae3dff444fb01b20e06b2c3fe3bf68
Vitaly-V <gtopbox@gmail.com>
2019-04-05 16:15:20 +0300

    Validation, Focus

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -21,8 +21,8 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
     return TextFormField(
       decoration: InputDecoration(labelText: 'Product Title'),
       validator: (String value) {
-        if (value.isEmpty) {
-          return 'Title is required';
+        if (value.isEmpty || value.length < 5) {
+          return 'Title is required and should be 5+ characters long';
         }
       },
       onSaved: (String value) {
@@ -37,6 +37,11 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
     return TextFormField(
       maxLines: 4,
       decoration: InputDecoration(labelText: 'Product Description'),
+      validator: (String value) {
+        if (value.isEmpty || value.length < 10) {
+          return 'Description is required and should be 10+ characters long';
+        }
+      },
       onSaved: (String value) {
         setState(() {
           _descriptionValue = value;
@@ -49,6 +54,12 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
     return TextFormField(
       keyboardType: TextInputType.number,
       decoration: InputDecoration(labelText: 'Product Price'),
+      validator: (String value) {
+        if (value.isEmpty ||
+            !RegExp(r'^(?:[1-9]\d*|0)?(?:\.\d+)?$').hasMatch(value)) {
+          return 'Price is requried and should be a number';
+        }
+      },
       onSaved: (String value) {
         setState(() {
           _priceValue = double.parse(value);
@@ -58,7 +69,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   }
 
   void _submitForm() {
-    if(!_formKey.currentState.validate()) {
+    if (!_formKey.currentState.validate()) {
       return;
     }
     _formKey.currentState.save();
@@ -77,25 +88,29 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
     final double deviceWidth = MediaQuery.of(context).size.width;
     final double targetWidth = deviceWidth > 550.0 ? 500 : deviceWidth * 0.95;
     final double targetPadding = deviceWidth - targetWidth;
-    return Container(
-      margin: EdgeInsets.all(10.0),
-      child: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
-          children: <Widget>[
-            _buildTitleTextField(),
-            _buildDescriptionTextField(),
-            _buildPriceTextField(),
-            SizedBox(
-              height: 10.0,
-            ),
-            RaisedButton(
-              child: Text('Save'),
-              textColor: Colors.white,
-              onPressed: _submitForm,
-            ),
-            /*GestureDetector(
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
+            children: <Widget>[
+              _buildTitleTextField(),
+              _buildDescriptionTextField(),
+              _buildPriceTextField(),
+              SizedBox(
+                height: 10.0,
+              ),
+              RaisedButton(
+                child: Text('Save'),
+                textColor: Colors.white,
+                onPressed: _submitForm,
+              ),
+              /*GestureDetector(
             onTap: _submitForm,
             child: Container(
               color: Colors.green,
@@ -103,7 +118,8 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
               child: Text('My Button'),
             ),
           ),*/
-          ],
+            ],
+          ),
         ),
       ),
     );

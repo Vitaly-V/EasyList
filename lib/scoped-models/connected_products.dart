ab3821beb7828cdc6b7d92ea2624962f0c69ff8e
Vitaly-V <gtopbox@gmail.com>
2019-04-18 16:45:04 +0300

    Add favorites handling with firebase

--- lib/scoped-models/connected_products.dart
+++ lib/scoped-models/connected_products.dart
@@ -176,7 +176,11 @@ mixin ProductsModel on ConnectedProductsModel {
             image: productData['image'],
             price: productData['price'],
             userEmail: productData['userEmail'],
-            userId: productData['userId']);
+            userId: productData['userId'],
+            isFavorite: productData['wishlistUsers'] == null
+                ? false
+                : (productData['wishlistUsers'] as Map<String, dynamic>)
+                    .containsKey(_authenticatedUser.id));
         fetchedProductList.add(product);
       });
       _products = fetchedProductList;
